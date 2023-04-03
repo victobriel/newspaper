639bd6a456899456d447bbc8d50e787146b3f304
victobriel <victorgbrielsilva@gmail.com>
2023-04-03 19:10:25 -0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+let date = document.getElementById("date");
+
+let weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let d = new Date();
+
+let day = d.getDay();
+
+let dayd = d.getDate();
+
+let month = d.getMonth();
+
+let year = d.getFullYear();
+
+let dayName = weekdays[day];
+
+date.innerText = `${dayName}, ${month + 1} ${year}`;
