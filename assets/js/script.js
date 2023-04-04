8e9043ae52c23571372991fb338798931cb4a9f2
victobriel <victorgbrielsilva@gmail.com>
2023-04-04 08:26:46 -0300

    Update newspaper

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,15 +11,8 @@ let weekdays = [
 ];
 
 let d = new Date();
-
 let day = d.getDay();
-
-let dayd = d.getDate();
-
 let month = d.getMonth();
-
 let year = d.getFullYear();
-
 let dayName = weekdays[day];
-
 date.innerText = `${dayName}, ${month + 1} ${year}`;
